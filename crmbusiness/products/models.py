80320d917037281f36b63d6be53eee14fde17612
ykozhanov <ykozhanov97@gmail.com>
2024-12-22 17:58:32 +0500

    correct negative decimal in products.models

--- crmbusiness/products/models.py
+++ crmbusiness/products/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True, default='')
-    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    cost = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
 
     def __str__(self):
         return self.name
