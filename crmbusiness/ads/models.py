b0732c128bb229b41991159bca2ec7732f23dece
ykozhanov <ykozhanov97@gmail.com>
2024-12-22 18:16:43 +0500

    Correct null

--- crmbusiness/ads/models.py
+++ crmbusiness/ads/models.py
@@ -5,7 +5,7 @@ from products.models import Product
 
 
 class PromotionChannel(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Имя")
+    name = models.CharField(max_length=100, verbose_name="Имя", null=False)
 
     class Meta:
         verbose_name = "Канал продвижения"
@@ -16,7 +16,7 @@ class PromotionChannel(models.Model):
 
 
 class Advertising(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=False)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False)
     promotion_channel = models.ManyToManyField(PromotionChannel, related_name='advertising')
     budget = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
